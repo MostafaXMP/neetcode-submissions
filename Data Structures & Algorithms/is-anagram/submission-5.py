767801a634dccbfe0324afa26aa1452bada3567e
MostafaXMP <203040mostafa203040@gmail.com>
2026-07-28 09:50:24 +0300

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        char_freq_s, char_freq_t = {}, {}
+
+        for i in range(len(s)):
+            char_freq_s[s[i]] = char_freq_s.get(s[i],0) + 1
+            char_freq_t[t[i]] = char_freq_t.get(t[i],0) + 1
+        return char_freq_s == char_freq_t
+
+        
